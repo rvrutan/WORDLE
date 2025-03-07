d2cbf4ed1c69d3ca22912d196c091fea6df80e53
rvrutan <rutanroni@gmail.com>
2025-03-06 22:19:52 -0500

    functions created, passed all the test

---
+++ index.spec.js
@@ -0,0 +1,67 @@
+const { Wordle, GREEN, YELLOW, BLACK } = require("./wordle.js");
+
+describe("Wordle", () => {
+  it("if guess has a diffirent number of letters than wordle, it should retun empty array", () => {
+    const wordle = new Wordle("alert");
+    const result = wordle.checkWord("boom");
+    expect(result).toEqual([]);
+  });
+  it("if guess matches the wordle, return array of all greens", () => {
+    const wordle = new Wordle("alert");
+    const result = wordle.checkWord("alert");
+    expect(result).toEqual([GREEN, GREEN, GREEN, GREEN, GREEN]);
+  });
+  it("if first letter is in the right position return green for that position", () => {
+    const wordle = new Wordle("alert");
+    const result = wordle.checkWord("abbbb");
+    expect(result).toEqual([GREEN, BLACK, BLACK, BLACK, BLACK]);
+  });
+  it("if last letter in the right position return green for that position", () => {
+    const wordle = new Wordle("alerts");
+    const result = wordle.checkWord("bbbbbs");
+    expect(result).toEqual([BLACK, BLACK, BLACK, BLACK, BLACK, GREEN]);
+  });
+  it("if letter exist but in the wrong position return yellow", () => {
+    const wordle = new Wordle("alert");
+    const result = wordle.checkWord("bbabb");
+    expect(result).toEqual([BLACK, BLACK, YELLOW, BLACK, BLACK]);
+  });
+  it("if letter exist only one time on the wordle return the second letter black", () => {
+    const wordle = new Wordle("alert");
+    const result = wordle.checkWord("abbab");
+    expect(result).toEqual([GREEN, BLACK, BLACK, BLACK, BLACK]);
+  });
+  it("if two of the same letter exist in the wordle and in the guess", () => {
+    const wordle = new Wordle("alera");
+    const result = wordle.checkWord("abbab");
+    expect(result).toEqual([GREEN, BLACK, BLACK, YELLOW, BLACK]);
+  });
+  it("if two of the same letter exist in the guess but only one in the wordle but position doesnt match", () => {
+    const wordle = new Wordle("alert");
+    const result = wordle.checkWord("babab");
+    expect(result).toEqual([BLACK, YELLOW, BLACK, BLACK, BLACK]);
+  });
+  it("if no letters part of the wordle return all black", () => {
+    const wordle = new Wordle("alert");
+    const result = wordle.checkWord("bbbbb");
+    expect(result).toEqual([BLACK, BLACK, BLACK, BLACK, BLACK]);
+  });
+
+  describe("letterRepeatedInGuess", () => {
+    it("guess word has no repeated letters", () => {
+      const wordle = new Wordle("alert");
+      const result = wordle.letterRepeatedInGuess("abcde", 0);
+      expect(result).toEqual(false);
+    });
+    it("guess word has repeated letters but only one exist in wordle", () => {
+      const wordle = new Wordle("alert");
+      const result = wordle.letterRepeatedInGuess("abate", 2);
+      expect(result).toEqual(true);
+    });
+    it("guess word has repeated letters but only one exist in wordle", () => {
+        const wordle = new Wordle("alert");
+        const result = wordle.letterRepeatedInGuess("abate", 0);
+        expect(result).toEqual(false);
+      });
+  });
+});
---
+++ wordle.js
@@ -0,0 +1,62 @@
+const GREEN = "g";
+const YELLOW = "y";
+const BLACK = "b";
+
+class Wordle {
+  word;
+  constructor(word) {
+    this.word = word;
+  }
+
+  checkWord(guess) {
+    if (guess.length !== this.word.length) {
+      return [];
+    }
+    if (guess === this.word) {
+      return [GREEN, GREEN, GREEN, GREEN, GREEN];
+    }
+    let result = [];
+
+    for (let i = 0; i < guess.length; i++) {
+      if (guess[i] === this.word[i]) {
+        result.push(GREEN);
+      } else if (this.word.includes(guess[i])) {
+        if (this.letterRepeatedInGuess(guess, i)) {
+          result.push(BLACK);
+        } else {
+          result.push(YELLOW);
+        }
+      } else {
+        result.push(BLACK);
+      }
+    }
+    return result;
+  }
+  
+  letterRepeatedInGuess(guess, index) {
+    let charCountInGuess = [];
+    for (let i = 0; i < guess.length; i++) {
+      if (guess[i] === guess[index]) {
+        charCountInGuess.push(i);
+      }
+    }
+    let charCountInWordle = [];
+    for (let i = 0; i < this.word.length; i++) {
+      if (this.word[i] === guess[index]) {
+        charCountInWordle.push(i);
+      }
+    }
+    if (charCountInGuess.length === 1) {
+      return false;
+    }
+    if (charCountInGuess.length === charCountInWordle.length) {
+      return false;
+    }
+    if (index === charCountInGuess[0]) {
+      return false;
+    }
+    return true;
+  }
+}
+
+module.exports = { Wordle, GREEN, YELLOW, BLACK };
