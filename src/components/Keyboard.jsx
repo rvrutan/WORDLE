01508bd204cef67fcc8ec960e5dc1e628c3f1112
rvrutan <rutanroni@gmail.com>
2025-03-17 21:48:24 -0400

    keyboard color change implemented

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,4 +1,4 @@
-import { createGrid } from "../tools/getColor";
+import { createGrid, getColorClass } from "../tools/getColor";
 
 function Keyboard({ handleKeyPress, guesses, currentGuess }) {
   const rows = [
@@ -10,12 +10,14 @@ function Keyboard({ handleKeyPress, guesses, currentGuess }) {
   const grid = createGrid(guesses, currentGuess);
 
   function getKeyColor(letter) {
-    //look at each guess in the grid
-    //look at each letter in the guess
-    //see if the given letter is in the guess
-    //if not go to the next guess
-    //if it is return the color
-    //else return null
+    for (let guess of guesses) {
+      for (let letterObj of guess) {
+        if (letterObj.letter === letter) {
+          return getColorClass(letterObj.color);
+        }
+      }
+    }
+    return "";
   }
 
   return (
@@ -26,7 +28,9 @@ function Keyboard({ handleKeyPress, guesses, currentGuess }) {
             <button
               key={key}
               onClick={() => handleKeyPress(key)}
-              className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-4 px-3 sm:py-2 sm:px-4 rounded-md transition-colors duration-200 ease-in-out text-xs sm:text-base uppercase"
+              className={`bg-gray-300 hover:bg-gray-400 text-black font-bold py-4 px-3 sm:py-2 sm:px-4 rounded-md transition-colors duration-200 ease-in-out text-xs sm:text-base uppercase ${getKeyColor(
+                key
+              )}`}
             >
               {key}
             </button>
